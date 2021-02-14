52b35f8a41d614980862cf119492bff1f78d918f
keisuke-iizuka <keisuke.iizuka@flip.inc>
2021-02-14 23:16:39 +0900

    🐛 fixed decoder in graph mode

--- tensorflow/run.py
+++ tensorflow/run.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     encoded_lens = tf.constant([10, 10])
 
     inference = GreedyInference(
-        predictor, joint, blank_index=0, max_symbols_per_step=30
+        predictor, joint, blank_index=0, max_symbols_per_step=10
     )
 
     # inference(encoded_outs, encoded_lens)
