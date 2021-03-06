72bb2e8551c749e612319dfe3aabc4f112aceda8
keisuke-iizuka <keisuke.iizuka@flip.inc>
2021-03-06 18:32:49 +0900

    ✨ added support for min_lr for lr_scheduler

--- tensorflow/trainer.py
+++ tensorflow/trainer.py
@@ -6,8 +6,8 @@ import tensorflow as tf
 tf.keras.backend.clear_session()
 
 if __name__ == "__main__":
-    initialize(config_path="../configs/emformer", job_name="emformer")
-    cfgs = compose(config_name="librispeech_char_mini_vgg.yml")
+    initialize(config_path="../configs/emformer", job_name="rnnt")
+    cfgs = compose(config_name="librispeech_char_mini_stack.yml")
 
     if "mxp" in cfgs.trainer:
         # TODO: support mixed precision training. All layers must adapt to
